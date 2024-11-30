457b8d03aba3ca7df3844a26af28bb0ac4dbddec
0110k011 <gmateust@gmail.com>
2024-11-30 19:19:11 -0300

    init project

---
+++ src/test/java/com/api/kapp/KappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.kapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
