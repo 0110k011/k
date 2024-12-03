b013e8cae4006151ef8e01668b9ba8b37ae7e41f
0110k011 <gmateust@gmail.com>
2024-12-03 14:55:24 -0300

    add account and transaction models

---
+++ src/main/java/com/api/k/models/AccountModel.java
@@ -0,0 +1,47 @@
+package com.api.k.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_account")
+@Getter
+@Setter
+public class AccountModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @OneToMany(mappedBy = "account", fetch = FetchType.LAZY)
+    private Set<TransactionModel> transactions = new HashSet<>();
+
+    @Column(nullable = false)
+    private String organization;
+
+    @Column(nullable = false)
+    private String currency;
+
+    @Column(nullable = false)
+    private String bankId;
+
+    @Column(nullable = false)
+    private String branchId;
+
+    @Column(nullable = false, unique = true)
+    private String accountId;
+
+    @Column(nullable = false)
+    private String accountType;
+
+}
---
+++ src/main/java/com/api/k/models/TransactionModel.java
@@ -0,0 +1,36 @@
+package com.api.k.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_transaction")
+@Getter
+@Setter
+public class TransactionModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    //@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ManyToOne//(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id")
+    private AccountModel account;
+
+    @Column(nullable = false)
+    private String transactionId;
+
+    @Column(nullable = false)
+    private String amount;
+
+    @Column(nullable = false)
+    private String date;
+
+}
